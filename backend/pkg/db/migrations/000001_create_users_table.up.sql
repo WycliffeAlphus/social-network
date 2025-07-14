f065d273408bc4da24873d443718b5f3d36943cd
David Ochiel <nguonodave@gmail.com>
2025-07-14 23:02:20 +0300

    refactor(schema): remove sessions table since it is implemented in PR #49

--- backend/pkg/db/migrations/000001_create_users_table.up.sql
+++ backend/pkg/db/migrations/000001_create_users_table.up.sql
@@ -11,11 +11,3 @@ CREATE TABLE users (
     profileVisibility TEXT NOT NULL DEFAULT 'public' CHECK(profileVisibility IN ('public', 'private')),
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE sessions (
-    id TEXT PRIMARY KEY, -- UUID v4, the session token
-    user_id TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    expires_at DATETIME NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
