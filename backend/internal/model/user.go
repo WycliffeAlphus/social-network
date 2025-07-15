702e6b8e2220588eefe89e79ac3e67fb688c2ea0
quinter <apondi-art>
2025-07-15 14:51:03 +0300

    chore(model): align User struct with database schema
    - Add db tags to User struct fields to match database column names:
      * fname -> first_name
      * lname -> last_name
      * profileVisibility -> profile_visibility
      * etc.
    - Maintain existing json tags for API compatibility
    - Ensure all fields have proper database mappings

--- backend/internal/model/user.go
+++ backend/internal/model/user.go
@@ -3,15 +3,15 @@ package model
 import "time"
 
 type User struct {
-	ID                string    `json:"id"`
-	Email             string    `json:"email"`
-	FirstName         string    `json:"first_name"`
-	LastName          string    `json:"last_name"`
-	DOB               time.Time `json:"dob"`
-	ImgURL            string    `json:"img_url,omitempty"`
-	Nickname          string    `json:"nickname,omitempty"`
-	About             string    `json:"about,omitempty"`
-	Password          string    `json:"password"`
-	ProfileVisibility string    `json:"profile_visibility"`
-	CreatedAt         time.Time `json:"created_at"`
+	ID                string    `json:"id" db:"id"`
+	Email             string    `json:"email" db:"email"`
+	FirstName         string    `json:"first_name" db:"fname"`
+	LastName          string    `json:"last_name" db:"lname"`
+	DOB               time.Time `json:"dob" db:"dob"`
+	ImgURL            string    `json:"img_url,omitempty" db:"imgurl"`
+	Nickname          string    `json:"nickname,omitempty" db:"nickname"`
+	About             string    `json:"about,omitempty" db:"about"`
+	Password          string    `json:"password" db:"password"`
+	ProfileVisibility string    `json:"profile_visibility" db:"profileVisibility"`
+	CreatedAt         time.Time `json:"created_at" db:"created_at"`
 }
