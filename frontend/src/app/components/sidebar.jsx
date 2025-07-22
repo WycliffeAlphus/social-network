fe80a258466f3a48b964ce72832bc56757f63868
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-07-22 12:45:11 +0300

    ref:remove duplicate friends icon

--- frontend/src/app/components/sidebar.jsx
+++ frontend/src/app/components/sidebar.jsx
@@ -17,37 +17,37 @@ export default function Sidebar() {
   return (
     <aside className="w-1/5 bg-blue-900 text-white min-h-full">
       {isGroupsPage ? (
-        <div>
-          <h2 className="text-lg font-bold mb-4 text-center">Groups</h2>
-          <ul className="space-y-2">
-            <li>
-              <SearchBar />
-            </li>
-            <li>
-              <Link
-                href="/create-group"
-                className="block px-4 py-2 flex  text-white hover:bg-blue-700 hover:shadow-md transition duration-200"
-              >
-                <PlusIcon className="h-6 w-6 mr-1" />
-                Create Group
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/groups"
-                className="block px-4 py-2 r  text-white hover:bg-blue-700 hover:shadow-md transition duration-200"
-              >
-                Your Groups
-              </Link>
-            </li>
-          </ul>
-        </div>
+           <div>
+      <h2 className="text-lg font-bold mb-4 text-center">Groups</h2>
+      <ul className="space-y-2">
+        <li>
+          <SearchBar />
+        </li>
+        <li>
+          <Link
+            href="/create-group"
+            className="block px-4 py-2 flex  text-white hover:bg-blue-700 hover:shadow-md transition duration-200"
+          >
+            <PlusIcon className="h-6 w-6 mr-1"/>
+            Create Group
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/groups"
+            className="block px-4 py-2 r  text-white hover:bg-blue-700 hover:shadow-md transition duration-200"
+          >
+            Your Groups
+          </Link>
+        </li>
+      </ul>
+    </div>
       ) : (
         <ul className="w-full">
           <li className="p-4 border-b border-gray-100/20">
             <Link href="/" className="flex w-full  pb-4 items-center">
               <HomeIcon className="h-6 w-6 mr-2" /> Home
-            </Link> <SearchBar />
+            </Link> <SearchBar/>
           </li>
           <li className="p-4 border-b border-gray-100/20">
             <Link href="/profile" className="flex w-full  pb-4 items-center">
@@ -63,14 +63,6 @@ export default function Sidebar() {
             <Link href="/friends" className="flex w-full pb-4 items-center">
               <div className="relative w-6 h-6 mr-2">
                 <UserGroupIcon className="h-5 w-5 text-white" />
-                <li className="p-4 border-b border-gray-100/20">
-                  <Link href="/friends" className="flex w-full pb-4 items-center">
-                    <div className="relative w-6 h-6 mr-2">
-                      <UserGroupIcon className="h-5 w-5 text-white" />
-                    </div>
-                    Friends
-                  </Link>
-                </li>
               </div>
               Friends
             </Link>
