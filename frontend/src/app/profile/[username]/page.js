f6214ffe4741301da8ffa121b117181c7b438622
quinter <apondi-art>
2025-07-24 08:56:57 +0300

    ifx:remove serverside props

--- frontend/src/app/profile/[username]/page.js
+++ frontend/src/app/profile/[username]/page.js
@@ -1,31 +1,26 @@
-import { getUser, getFollowers } from '../../utils/api';
-import ProfileCard from '../../components/ProfileCard';
-import FollowerCard from '../../components/FollowerCard';
+import ProfileCard from "@/app/components/ProfileCard";
+import FollowerCard from "@/app/components/FollowerCard";
+import { getUser, getFollowers } from "@/app/utils/api";
 
-export default function ProfilePage({ user, followers }) {
+export default async function ProfilePage({ params }) {
+  const username = params.username;
+
+  // ✅ Fetch real data from your Go backend
+  const [user, followers] = await Promise.all([
+    getUser(username),
+    getFollowers(username),
+  ]);
+
   return (
-    <div>
+    <div style={{ maxWidth: "600px", margin: "0 auto", padding: "20px" }}>
       <ProfileCard user={user} />
 
-      <h3 style={{ marginTop: '30px' }}>Followers</h3>
+      <h3 style={{ marginTop: "30px" }}>Followers</h3>
       <div>
-        {followers.map(f => (
+        {followers.map((f) => (
           <FollowerCard key={f.username} user={f} isFollowing={f.isFollowing} />
         ))}
       </div>
     </div>
   );
-}
-
-export async function getServerSideProps(context) {
-  const { username } = context.params;
-
-  const [user, followers] = await Promise.all([
-    getUser(username),
-    getFollowers(username)
-  ]);
-
-  return {
-    props: { user, followers }
-  };
 }
