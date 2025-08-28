d2bcef2e9382f79f5c147499398aa759650cc57b
David Ochiel <nguonodave@gmail.com>
2025-08-28 06:47:57 +0300

    feat: implement frontend API call for fetching and rendering contacted users

--- frontend/src/app/messages/layout.js
+++ frontend/src/app/messages/layout.js
@@ -4,13 +4,13 @@ import React from "react";
 
 export default function MessagesLayout({ children }) {
     return (
-        <div className="bg-red-900 h-screen flex">
-            <div className="bg-blue-900 opacity-50 w-[30%] border-x border-gray-200 overflow-auto scrollbar p-4">
-                <h2 className="font-semibold mb-12">Contacts</h2>
+        <div className="h-screen flex">
+            <div className="w-[30%] border-x border-gray-400 overflow-auto scrollbar dark:scrollbar-dark p-2">
+                <h2 className="font-semibold mb-7 p-4">Contacts</h2>
                 <ContactedChats />
             </div>
 
-            <div className="bg-green-900 opacity-50 flex-1 px-4">
+            <div className="flex-1 border-r border-gray-400">
                 {children}
             </div>
         </div>
--- frontend/src/components/contacts.jsx
+++ frontend/src/components/contacts.jsx
@@ -1,91 +1,87 @@
 'use client';
 
-// import { useEffect, useState } from 'react';
-// import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Loading from './loading';
+import { useParams } from 'next/navigation';
 
 export function ContactedChats() {
-  // const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const { receiverId } = useParams()
 
-  // useEffect(() => {
-  //   async function fetchContacts() {
-  //     const response = await fetch('/api/contacts');
-  //     const data = await response.json();
-  //     setContacts(data);
-  //   }
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/users', {
+          credentials: 'include',
+        });
 
-  //   fetchContacts();
-  // }, []);
+        if (!response.ok) {
+          throw new Error('Failed to fetch contacted users')
+        }
+
+        const data = await response.json();
+
+        setContacts(data);
+      } catch (error) {
+        setError(error.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchContacts();
+  }, []);
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (error) {
+    return <div className="text-red-500">{error}</div>
+  }
 
   return (
-    <div className="">
-        <p>some contact vvvvvvvvvvvvvvvvvvv</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
-        <p>some contact</p>
+    <div className="flex flex-col gap-3">
+      {contacts && contacts.length > 0 ? (
+        contacts.map((contact) => (
+          <Link
+            key={contact.id}
+            href={`/messages/${contact.id}`}
+            className={`p-3 flex items-center gap-3 transition-colors rounded-xl
+            ${receiverId === contact.id
+                ? 'bg-gray-200 dark:bg-gray-600'
+                : 'hover:bg-gray-600/30'
+              }
+            `}
+          >
+            {contact.avatar?.String ? (
+              <img
+                src={contact.avatar.String}
+                alt="User avatar"
+                className="w-12 h-12 rounded-full object-cover"
+              />
+            ) : (
+              <div className="w-[clamp(2rem,4vw,3.5rem)] h-[clamp(2rem,4vw,3.5rem)] rounded-full bg-gray-200 overflow-hidden flex items-center justify-center">
+                <span className="text-lg text-black font-medium">
+                  {contact.firstname?.charAt(0).toUpperCase()}
+                  {contact.lastname?.charAt(0).toUpperCase()}
+                </span>
+              </div>
+            )}
+
+            <div>
+              <span className="font-medium">
+                {contact.firstname} {contact.lastname}
+              </span>
+            </div>
+          </Link>
+        ))
+      ) : (
+        <p>You haven't messaged anyone yet.</p>
+      )}
     </div>
   );
-}
+} 
