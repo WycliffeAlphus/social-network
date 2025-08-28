209de5e52e2717c2a05037450569b1572e5328d5
David Ochiel <nguonodave@gmail.com>
2025-08-28 10:20:43 +0300

    feat(dependencies): add package for emoji picker

--- frontend/src/app/messages/page.js
+++ frontend/src/app/messages/page.js
@@ -1,7 +1,7 @@
 export default function MessagesPage() {
   return (
-    <div className="flex h-full items-center">
-      <div>
+    <div className="flex h-full items-center justify-center">
+      <div className="w-[77%]">
         <h2 className="font-semibold">Select a user to chat with</h2>
         <p className="">
           Choose a contact from the sidebar to start a conversation
