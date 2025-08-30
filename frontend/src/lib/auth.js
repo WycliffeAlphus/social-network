2cb4aeec2fba64a3205a3635929a4ddd3366b51e
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-08-30 14:43:54 +0300

    feat(frontend): Improve error display and post creation redirection

--- frontend/src/lib/auth.js
+++ frontend/src/lib/auth.js
@@ -2,21 +2,23 @@ export const showFieldError = (fieldId, message) => {
     const errorElement = document.getElementById(`${fieldId}-error`);
     const inputElement = document.getElementById(fieldId);
 
-    if (message) {
-        errorElement.textContent = message;
-        errorElement.style.display = 'block';
-        if (fieldId != "title" && fieldId != "content") {
-            inputElement.classList.add('input-error');
+    if (errorElement) {
+        if (message) {
+            errorElement.textContent = message;
+            errorElement.style.display = 'block';
+            if (fieldId != "title" && fieldId != "content") {
+                if (inputElement) inputElement.classList.add('input-error');
+            } else {
+                if (inputElement) inputElement.classList.add('input-error-bottom');
+            }
         } else {
-            inputElement.classList.add('input-error-bottom');
-        }
-    } else {
-        errorElement.textContent = '';
-        errorElement.style.display = 'none';
-        if (fieldId != "title" && fieldId != "content") {
-            inputElement.classList.remove('input-error');
-        } else {
-            inputElement.classList.remove('input-error-bottom');
+            errorElement.textContent = '';
+            errorElement.style.display = 'none';
+            if (fieldId != "title" && fieldId != "content") {
+                if (inputElement) inputElement.classList.remove('input-error');
+            } else {
+                if (inputElement) inputElement.classList.remove('input-error-bottom');
+            }
         }
     }
 }
